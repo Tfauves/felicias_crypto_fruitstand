0b2ca185dc1a2edd04b86b254d80de6aa800c885
Tranquility Base <tfauvelle@gmail.com>
2022-07-05 12:33:13 -0400

    test impl and passing

--- tests/felicias_crypto_fruitstand.ts
+++ tests/felicias_crypto_fruitstand.ts
@@ -19,4 +19,62 @@ describe("felicias_crypto_fruitstand", () => {
     .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Adds to Inventory", async () => {
+    // Add your test here.
+    const tx = await program.methods.manageInventory({add: {}}, new anchor.BN(5))
+    .accounts({
+      inventory: kp.publicKey,
+      payer: kp.publicKey,
+    })
+    .signers([kp])
+    .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Subtracts from Inventory", async () => {
+    // Add your test here.
+    const tx = await program.methods.manageInventory({subtract: {}}, new anchor.BN(3))
+    .accounts({
+      inventory: kp.publicKey,
+      payer: kp.publicKey,
+    })
+    .signers([kp])
+    .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Multiplys Inventory", async () => {
+    // Add your test here.
+    const tx = await program.methods.manageInventory({multiply: {}}, new anchor.BN(2))
+    .accounts({
+      inventory: kp.publicKey,
+      payer: kp.publicKey,
+    })
+    .signers([kp])
+    .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Divides Inventory", async () => {
+    // Add your test here.
+    const tx = await program.methods.manageInventory({divide: {}}, new anchor.BN(2))
+    .accounts({
+      inventory: kp.publicKey,
+      payer: kp.publicKey,
+    })
+    .signers([kp])
+    .rpc();
+    console.log("Your transaction signature", tx);
+  
+  });
+
+
+
+
+
+
+
+
+
 });
